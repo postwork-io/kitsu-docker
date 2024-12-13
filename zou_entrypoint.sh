5194f18dac875bfc411ae527347f517ffafdd727
Nathan Inglesby <nate@postwork.io>
2024-12-13 09:09:14 -0500

    run init script on api only

--- zou_entrypoint.sh
+++ zou_entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-if [ ! -f "/opt/zou/tmp/initialized" ]; then
-    /bin/bash touch /opt/zou/tmp/initialized
-    /bin/bash -c "./first_run.sh ${ADMIN_USERNAME} ${ADMIN_PASSWORD}"
-fi
+
+. zouenv/bin/activate
+
+
 if [ "$1" == "api" ]; then
+
+    if [ ! -f "/opt/zou/tmp/initialized" ]; then
+        /bin/bash -c touch /opt/zou/tmp/initialized
+        /bin/bash -c "./first_run.sh ${ADMIN_USERNAME} ${ADMIN_PASSWORD}"
+    fi
     echo "Starting Zou API server"
     /bin/bash -c "/opt/zou/zouenv/bin/gunicorn  -c /etc/zou/gunicorn.conf.py -b ${hostname}:5000 zou.app:app"
 
